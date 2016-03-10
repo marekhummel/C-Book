a0cc173b75a44a5b4097c0d1226e79dd7284fb69
Marek Hummel <Pl3x14n@users.noreply.github.com>
2016-03-10 18:56:23 +0100

    Appointment overview CSS

--- DateTime.rb
+++ DateTime.rb
@@ -3,30 +3,48 @@
 class DateTime
 
     def Year    
-        self.strftime("%Y").to_i
+        return self.strftime("%Y").to_i
     end
 
     def Month
-        self.strftime("%m").to_i
+        return self.strftime("%m").to_i
     end
 
     def Day
-        self.strftime("%d").to_i
+        return self.strftime("%d").to_i
     end
 
     def Hours
-        self.strftime("%H").to_i
+        return self.strftime("%H").to_i
     end
 
     def Minutes
-        self.strftime("%M").to_i
+        return self.strftime("%M").to_i
     end
 
     def Seconds
-        self.strftime("%S").to_i
+        return self.strftime("%S").to_i
     end
 
-    def Format
-        self.strftime("%d. %m. %Y - %H:%M:%S")
+    def DayOfTheWeek
+        i = self.strftime("%w").to_i
+        days = ["Sonntag", "Montag", "Dienstag", "MIttwoch", "Donnerstag", "Freitag", "Samstag"]
+
+        return days[i]
     end
+
+
+
+    def FullDate
+        return self.DayOfTheWeek + ", " + self.strftime("%d.%m.%Y")
+    end
+
+    def FullTime 
+        return self.strftime("%H:%M") + " Uhr"
+    end
+
+    def Format!
+       return self.FullDate + " - " + self.FullTime
+    end
+
 end 
